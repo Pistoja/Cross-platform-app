12b3c1c4646f4084ecd03ed0ddf964d179f5ae91
Peter Gira <49673257+Pistoja@users.noreply.github.com>
2019-11-18 16:31:02 +0100

    add maps

--- src/data.js
+++ src/data.js
@@ -61,5 +61,51 @@ module.exports = {
         "desc": "bla bla bla caffè cornetto e amaro",
         "id": "77s@hs$"
       }
-    ]
+    ],
+    "map": {
+      "geojson": {
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "properties": {
+              "evento": "colazione"
+            },
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                13.762092590332031,
+                45.649268385921395
+              ]
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+            "evento": "Inizio"
+            },
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                13.759925365447998,
+                45.646283314737836
+              ]
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "evento": "fine"
+            },
+            "geometry": {
+              "type": "Point",
+              "coordinates": [
+                13.764474391937254,
+                45.64815837802974
+              ]
+            }
+          }
+        ]
+      }
+    }
   }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 const Calendar = require('./calendar.js')
 const Speakers = require('./speakers.js')
+const MyMap = require('./map.js')
+
 Calendar.render()
 Speakers.render()
-
-
-
-const myMap = L.map('mapView').setView(45.6522854, 13.7661518, 14)
-
-LtitleLayer
+MyMap.render()
